ed45a59b5036ae96a846081f615128df3e4e959a
Patrick Ferreira <patrickcorrea.f@gmail.com>
2023-05-20 14:39:39 -0300

    feature: create controller for balance

---
+++ src/controllers/balance.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, HttpStatus } from '@nestjs/common';
+import { BalanceService } from '../services/balance.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { BalanceDTO } from '../commons/dto/balance.dto';
+import { DailyBalanceDTO } from '../commons/dto/daily-balance.dto';
+
+@Controller('balance')
+@ApiTags('Balance')
+export class BalanceController {
+  constructor(private readonly balanceService: BalanceService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'Get All Balance Releases' })
+  @ApiResponse({ status: HttpStatus.OK, type: BalanceDTO, isArray: true })
+  getAll() {
+    return this.balanceService.getAll();
+  }
+
+  @Get('daily')
+  @ApiOperation({ summary: 'Get All Daily Releases of Balance' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: DailyBalanceDTO,
+    isArray: true,
+  })
+  getDaily() {
+    return this.balanceService.getDaily();
+  }
+}
